7923958e941249df73f4f58547cc1dee6a51d683
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-12-05 07:43:03 +0000

    Add deployment scripts

---
+++ scripts/codedeploy/application-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AWS_REGION=
+CONFIG_BUCKET=
+ECR_REPOSITORY_URI=
+GIT_COMMIT=
+
+INSTANCE=$(curl -s http://instance-data/latest/meta-data/instance-id)
+CONFIG=$(aws --region $AWS_REGION ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" "Name=key,Values=Configuration" --output text | awk '{print $5}')
+
+(aws s3 cp s3://$CONFIG_BUCKET/dp-csv-splitter/$CONFIG.asc . && gpg --decrypt $CONFIG.asc > $CONFIG) || exit $?
+
+source $CONFIG && docker run -d \
+  --name=dp-csv-splitter        \
+  --net=$DOCKER_NETWORK         \
+  --restart=always              \
+  $ECR_REPOSITORY_URI/dp-csv-splitter:$GIT_COMMIT
